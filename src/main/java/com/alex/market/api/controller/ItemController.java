a7afa1f03c9ad090dbacc065289c30071a499f22
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-20 20:36:07 +0300

    Added test webMvc for ItemController#

--- src/main/java/com/alex/market/api/controller/ItemController.java
+++ src/main/java/com/alex/market/api/controller/ItemController.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
@@ -18,14 +19,15 @@ import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping(value = {"/", "/items"})
 public class ItemController {
     private final ItemService itemService;
 
-    @GetMapping(value = {"/", "/items"})
+    @GetMapping
     public String getItems(@RequestParam(required = false) String search,
-                           @RequestParam(defaultValue = "NO") SortColumn sort,
+                           @RequestParam(required = false, defaultValue = "NO") SortColumn sort,
                            @RequestParam(required = false, defaultValue = "1") Integer pageNumber,
-                           @RequestParam(defaultValue = "10") Integer pageSize,
+                           @RequestParam(required = false, defaultValue = "10") Integer pageSize,
                            @SessionAttribute Map<Long, Integer> cart,
                            Model model) {
 
