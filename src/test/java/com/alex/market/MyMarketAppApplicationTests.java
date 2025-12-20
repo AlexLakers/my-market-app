faf9a7a5f70ba5abce63a659c014a6b53cb02a04
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-20 16:50:26 +0300

    Added sql scripts (data-test.sql, cleanup.sql) for test environment. I did it not to depends on other env.

--- src/test/java/com/alex/market/MyMarketAppApplicationTests.java
+++ src/test/java/com/alex/market/MyMarketAppApplicationTests.java
@@ -1,11 +1,20 @@
 package com.alex.market;
 
+import com.alex.market.config.PostgresTestconteinerConfig;
 import com.alex.market.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@ImportTestcontainers(PostgresTestconteinerConfig.class)
+@Transactional
 class MyMarketAppApplicationTests {
 
 	@Test
