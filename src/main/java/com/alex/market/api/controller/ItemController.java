f00d9c54ea65c95786b56d85c06dcca9f6109404
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-20 15:38:13 +0300

    Created ItemController:  1. enpoint search items using input params     GET /?search=[search]&sort=[sort]&pageNumber=[pageNumber]&pageSize=[pageSize]     Or     GET /items?search=[search]&sort=[sort]&pageNumber=[pageNumber]&pageSize=[pageSize]

---
+++ src/main/java/com/alex/market/api/controller/ItemController.java
@@ -0,0 +1,43 @@
+package com.alex.market.api.controller;
+
+import com.alex.market.search.PageItemsDto;
+import com.alex.market.search.SearchDto;
+import com.alex.market.search.SortColumn;
+import com.alex.market.service.ItemService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping(value = {"/", "/items"})
+    public String getItems(@RequestParam(required = false) String search,
+                           @RequestParam(defaultValue = "NO") SortColumn sort,
+                           @RequestParam(required = false, defaultValue = "1") Integer pageNumber,
+                           @RequestParam(defaultValue = "10") Integer pageSize,
+                           @SessionAttribute Map<Long, Integer> cart,
+                           Model model) {
+
+
+        PageItemsDto pageItemsDto = itemService.getItemsPage(new SearchDto(search, sort, pageNumber, pageSize, cart));
+
+        model.addAttribute("items", pageItemsDto.items());
+        model.addAttribute("search", pageItemsDto.search());
+        model.addAttribute("sort", pageItemsDto.sort());
+        model.addAttribute("paging", pageItemsDto.pageDto());
+
+        return "items";
+    }
+
+}
