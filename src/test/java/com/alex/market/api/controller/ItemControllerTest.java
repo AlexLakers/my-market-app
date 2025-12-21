4d189dc5692a53b5a95ce2bb42e9eae7257d3fb1
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-21 09:50:14 +0300

    1.Added test for endpoint GET '/items{id}'.

--- src/test/java/com/alex/market/api/controller/ItemControllerTest.java
+++ src/test/java/com/alex/market/api/controller/ItemControllerTest.java
@@ -2,17 +2,23 @@ package com.alex.market.api.controller;
 
 import com.alex.market.api.dto.ItemDto;
 import com.alex.market.api.dto.PageDto;
+import com.alex.market.exception.ItemNotFoundException;
+import com.alex.market.exception.handler.GlobalExceptionHandler;
+import com.alex.market.model.Item;
 import com.alex.market.search.PageItemsDto;
 import com.alex.market.search.SearchDto;
 import com.alex.market.search.SortColumn;
 import com.alex.market.service.ItemService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockReset;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,18 +28,28 @@ import java.util.*;
 
 
 @WebMvcTest(ItemController.class)
+@Import(GlobalExceptionHandler.class)
 @ActiveProfiles("test")
 class ItemControllerTest {
 
-    @MockitoBean
+    @MockitoBean(reset = MockReset.BEFORE)
     private ItemService itemService;
 
     @Autowired
     private MockMvc mockMvc;
 
+    private Map<Long, Integer> cartItemsCount;
+
+    private static final Long VALID_ID=1L;
+    private static final Long INVALID_ID=Long.MAX_VALUE;
+
+    @BeforeEach
+    void setUp() {
+        cartItemsCount= Map.of(1L,1,2L,2,3L,3,4L,4);
+    }
+
     @Test
     void getItems_shouldReturnViewAndModelWithData() throws Exception {
-        Map<Long, Integer> cartItemsCount = Map.of(1L,1,2L,2,3L,3,4L,4);
         PageItemsDto expectedDto=new PageItemsDto(Collections.emptyList(),"test",SortColumn.PRICE.name(),new PageDto(3,1,false,false));
 
         Mockito.when(itemService.getItemsPage(Mockito.any(SearchDto.class))).thenReturn(expectedDto);
@@ -51,7 +67,6 @@ class ItemControllerTest {
     }
     @Test
     void getItems_shouldSetParamsSomeDefaultValue_whenTheseParamsNotGiven() throws Exception {
-        Map<Long, Integer> cartItemsCount = Map.of(1L,1,2L,2,3L,3,4L,4);
         PageItemsDto expectedDto=new PageItemsDto(Collections.emptyList(),"test",SortColumn.PRICE.name(),new PageDto(3,1,false,false));
 
         Mockito.when(itemService.getItemsPage(Mockito.any(SearchDto.class))).thenReturn(expectedDto);
@@ -62,6 +77,27 @@ class ItemControllerTest {
                 .andExpect(MockMvcResultMatchers.view().name("items"))
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("text/html;charset=UTF-8")))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("items","search","sort","paging"));
+    }
+    @Test
+    void getItemById_shouldReturnViewAndModelWithDataSuccess() throws Exception {
+        ItemDto expectedDto=new ItemDto(VALID_ID, "testTitle1", "testDesc1", "testImagePath1", 1000L, cartItemsCount.get(VALID_ID));
+        Mockito.when(itemService.findByIdWithCartCount(VALID_ID,cartItemsCount)).thenReturn(expectedDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/items/{id}",VALID_ID)
+                        .sessionAttr("cart",cartItemsCount))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("item"))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("text/html;charset=UTF-8")))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("item"));
+    }
+
+    @Test
+    void getItemById_shouldThrowItemNotFoundException_whenItemNotFound() throws Exception {
+        Mockito.doThrow(new ItemNotFoundException(INVALID_ID)).when(itemService).findByIdWithCartCount(INVALID_ID,cartItemsCount);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/items/{id}",INVALID_ID)
+                        .sessionAttr("cart",cartItemsCount))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
 
