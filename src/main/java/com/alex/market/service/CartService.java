7c0bea06bf872c6dc23c9f8444e15c03f5b9981d
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-22 07:22:55 +0300

    1. I Added method CartService#getCartItems(Map cart) that allows to get items from the current user Cart and calculate total price.

--- src/main/java/com/alex/market/service/CartService.java
+++ src/main/java/com/alex/market/service/CartService.java
@@ -1,9 +1,12 @@
 package com.alex.market.service;
 
 import com.alex.market.api.dto.CartChangeDto;
+import com.alex.market.api.dto.CartDto;
 
 import java.util.Map;
 
 public interface CartService {
     Integer changeItemCount(CartChangeDto cartChangeDto);
+    CartDto getCartItems(Map<Long,Integer> cartItemsCount);
+
 }
