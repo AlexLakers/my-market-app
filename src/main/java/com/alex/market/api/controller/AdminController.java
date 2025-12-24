9e26fdc2a2dd69c8ebf0ad8b283fa946ef02e7a1
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-24 23:08:38 +0300

    I added AdminController 1.endpoint 'cart/items' to show page for creating new item 2. endpoint 'cart/items/add' to handle post request.

---
+++ src/main/java/com/alex/market/api/controller/AdminController.java
@@ -0,0 +1,46 @@
+package com.alex.market.api.controller;
+
+import com.alex.market.api.dto.ItemCreateDto;
+import com.alex.market.service.ItemService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
+
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/admin")
+@Validated
+public class AdminController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items")
+    public String adminPage() {
+        return "newItem";
+    }
+
+    @PostMapping(value = "/items/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String createItem(@Valid @ModelAttribute ItemCreateDto item,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes,
+                             Model model) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            redirectAttributes.addFlashAttribute("title", item.title());
+            redirectAttributes.addFlashAttribute("description", item.description());
+            redirectAttributes.addFlashAttribute("price", item.price());
+            return "redirect:/admin/items";
+        }
+        model.addAttribute("item", itemService.createItem(item));
+        return "item";
+    }
+}
--- src/main/java/com/alex/market/exception/TitleAlreadyExistsException.java
+++ src/main/java/com/alex/market/exception/TitleAlreadyExistsException.java
@@ -2,11 +2,11 @@ package com.alex.market.exception;
 
 public class TitleAlreadyExistsException extends RuntimeException {
     public TitleAlreadyExistsException(String title) {
-        super("The title: %1$s".formatted(title));
+        super("The title: %1$s is already exists".formatted(title));
     }
 
     public TitleAlreadyExistsException(String title, Throwable cause) {
-        super("The title: %1$s".formatted(title), cause);
+        super("The title: %1$s is already exists".formatted(title), cause);
     }
 
 
