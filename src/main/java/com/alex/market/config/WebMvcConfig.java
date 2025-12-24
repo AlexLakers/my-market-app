6e414de68fbdf16b22110368b68778ac7bb4580b
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-25 00:29:44 +0300

    I changed WebMvcConfig to override my own resource handler for images.

--- src/main/java/com/alex/market/config/WebMvcConfig.java
+++ src/main/java/com/alex/market/config/WebMvcConfig.java
@@ -1,19 +1,37 @@
 package com.alex.market.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.CacheControl;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.ResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.List;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer{
 
+    @Value("${market.upload.dir:/home/alexlakers/my-market/}")
+    private String baseDir;
+
+    @Override
     public  void addResourceHandlers(ResourceHandlerRegistry registry) {
-
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/images/");
-
-  /*      registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");*/
-
+                .addResourceLocations("file:"+baseDir+"/images/")
+                .setCacheControl(CacheControl.noCache())
+                .setCachePeriod(0)
+                .resourceChain(false);
     }
 }
