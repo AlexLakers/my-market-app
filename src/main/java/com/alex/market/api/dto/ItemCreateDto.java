90813f98c612b8a6b208b4123cb7e804e7f3a025
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-24 21:39:26 +0300

    I added Validator for image

--- src/main/java/com/alex/market/api/dto/ItemCreateDto.java
+++ src/main/java/com/alex/market/api/dto/ItemCreateDto.java
@@ -1,11 +1,10 @@
 package com.alex.market.api.dto;
 
-import com.alex.market.validation.ValidImage;
+import com.alex.market.validator.ValidImage;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.context.MessageSource;
 import org.springframework.web.multipart.MultipartFile;
 
 public record ItemCreateDto(@NotBlank String title,
