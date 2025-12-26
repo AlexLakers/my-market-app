767cf1c14c134c0f0ce748ca43f3fc0cd77071c5
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-27 00:38:17 +0300

    I deleted FileService -changed to ImageService

--- src/main/java/com/alex/market/service/FileService.java
+++
@@ -1,7 +0,0 @@
-package com.alex.market.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileService {
-    String saveFile(MultipartFile file, String fileName);
-}
