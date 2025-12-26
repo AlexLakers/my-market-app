788a97184b3271d2f65611e31b0a2a46790385b8
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-12-26 22:33:30 +0300

    I added AdminController with endpoints: 1.POST admin/items/add - create new item 2. GET admin/images - get page for uploading new image 3. POST admin/images/{id} - upload new image for corresponding item

--- src/main/java/com/alex/market/controller/AdminController.java
+++ src/main/java/com/alex/market/controller/AdminController.java
@@ -1,15 +1,17 @@
-package com.alex.market.api.controller;
+package com.alex.market.controller;
 
-import com.alex.market.api.dto.input.ItemCreateDto;
+import com.alex.market.dto.input.ItemCreateDto;
+import com.alex.market.dto.output.ItemDto;
+import com.alex.market.service.AdminService;
 import com.alex.market.service.ItemService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -19,18 +21,17 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Validated
 public class AdminController {
 
-    private final ItemService itemService;
+    private final AdminService adminService;
 
     @GetMapping("/items")
     public String adminPage() {
         return "newItem";
     }
 
-    @PostMapping(value = "/items/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/items/add")
     public String createItem(@Valid @ModelAttribute ItemCreateDto item,
                              BindingResult bindingResult,
-                             RedirectAttributes redirectAttributes,
-                             Model model) {
+                             RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
             redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
             redirectAttributes.addFlashAttribute("title", item.title());
@@ -38,7 +39,21 @@ public class AdminController {
             redirectAttributes.addFlashAttribute("price", item.price());
             return "redirect:/admin/items";
         }
-        model.addAttribute("item", itemService.createItem(item));
-        return "item";
+        ItemDto itemDto = adminService.createItem(item);
+        redirectAttributes.addFlashAttribute("item", itemDto);
+        return "redirect:/admin/images";
     }
+
+    @GetMapping("/images")
+    public String imagePage() {
+        return "newImage";
+    }
+
+    @PostMapping(value = "/images/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String uploadImage(@PathVariable Long id, @RequestPart("image") MultipartFile image) {
+        adminService.uploadImage(image, id);
+        return "redirect:/items/{id}";
+
+    }
+
 }
