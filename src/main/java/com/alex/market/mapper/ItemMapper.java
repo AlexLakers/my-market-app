aefe765d3c21c36b44b263b628460a79565fa41b
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-01-03 14:29:52 +0300

    1. I added ItemMapper using MapStruct 2.Added tests for it

---
+++ src/main/java/com/alex/market/mapper/ItemMapper.java
@@ -0,0 +1,34 @@
+package com.alex.market.mapper;
+
+
+import com.alex.market.dto.output.ItemDto;
+import com.alex.market.model.Item;
+import com.alex.market.model.OrderItem;
+import com.alex.market.repository.projection.OrderItemsDetails;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Map;
+
+@Mapper(componentModel = "spring")
+public interface ItemMapper {
+
+    @Mapping(target = "price", source = "item.price")
+    @Mapping(target = "count", source = "count")
+    ItemDto toDto(Item item, Integer count);
+
+    default ItemDto toDto(Item item, Map<Long, Integer> cart) {
+        return toDto(item, cart.getOrDefault(item.getId(), 0));
+    }
+
+  /*  @Mapping(target = "id", source = "item.id")
+    @Mapping(target = "title", source = "item.title")
+    @Mapping(target = "description", source = "item.description")
+    @Mapping(target = "imgPath", source = "item.imgPath")
+    @Mapping(target = "price", source = "historyPrice")
+    @Mapping(target = "count", source = "count")
+    ItemDto toDtoFromOrderItem(OrderItem orderItem);*/
+
+    ItemDto toDtoFromOrderItemDetails(OrderItemsDetails orderItemsDetails);
+}
+
