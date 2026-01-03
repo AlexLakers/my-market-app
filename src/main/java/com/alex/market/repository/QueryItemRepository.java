ca2a30c075c4caa448ba881d93d9a944cc24b14b
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-01-03 13:27:16 +0300

    1.I added QueryItemRepository to realize additional functional for search using filter and pagination.

---
+++ src/main/java/com/alex/market/repository/QueryItemRepository.java
@@ -0,0 +1,10 @@
+package com.alex.market.repository;
+
+import com.alex.market.model.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import reactor.core.publisher.Mono;
+
+public interface QueryItemRepository {
+    Mono<Page<Item>> findAll(String search, Pageable pageable);
+}
---
+++ src/main/java/com/alex/market/repository/impl/QueryItemRepositoryImpl.java
@@ -0,0 +1,68 @@
+package com.alex.market.repository.impl;
+
+import com.alex.market.model.Item;
+import com.alex.market.repository.QueryItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.relational.core.query.Criteria;
+import org.springframework.data.relational.core.query.Query;
+import org.springframework.util.StringUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public class QueryItemRepositoryImpl implements QueryItemRepository {
+
+    private final R2dbcEntityTemplate r2dbcEntityTemplate;
+
+    @Override
+    public Mono<Page<Item>> findAll(String search, Pageable pageable) {
+
+        Criteria criteria = buildSearchCriteria(search);
+
+        Mono<Long> count = r2dbcEntityTemplate.select(Item.class)
+                .matching(Query.query(criteria))
+                .count()
+                .defaultIfEmpty(0L);
+
+        Flux<Item> items = r2dbcEntityTemplate.select(Item.class)
+                .matching(Query.query(criteria)
+                        .with(pageable)).all();
+
+        return items.collectList()
+                .zipWith(count)
+                .map(tuple -> new PageImpl<Item>(tuple.getT1(), pageable, tuple.getT2()));
+    }
+      /*  Criteria criteria = buildSearchCriteria(search);
+
+        Flux<Item> content = r2dbcEntityTemplate.select(Item.class)
+                .matching(Query.query(criteria).with(pageable))
+                .all();
+
+        Mono<Long> count = r2dbcEntityTemplate.select(Item.class)
+                .matching(Query.query(criteria))
+                .count();
+
+        Mono<Integer> totalPages = count.map(c -> (int) Math.ceil((double) c / pageable.getPageSize()));
+
+        return new PageItems(
+                content,
+                count,
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                totalPages
+        );
+    }*/
+
+    private Criteria buildSearchCriteria(String search) {
+        if (!StringUtils.hasText(search)) {
+            return Criteria.empty();
+        }
+        String searchParam = "%" + search + "%";
+        return Criteria.where(Item.Fields.title).like(searchParam)
+                .or(Criteria.where(Item.Fields.description).like(searchParam));
+    }
+}
