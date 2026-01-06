05fe863337a7e9d818edec7eabb0de41dd405e2f
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-01-06 17:55:56 +0300

    I added test for OrderService#findOrderWithItems()

--- src/test/java/com/alex/market/service/OrderServiceTest.java
+++ src/test/java/com/alex/market/service/OrderServiceTest.java
@@ -2,13 +2,16 @@ package com.alex.market.service;
 
 import com.alex.market.dto.output.ItemDto;
 import com.alex.market.dto.output.OrderDto;
+import com.alex.market.exception.OrderNotFoundException;
 import com.alex.market.mapper.ItemMapper;
 import com.alex.market.mapper.ItemMapperImpl;
+import com.alex.market.model.Item;
 import com.alex.market.model.Order;
 import com.alex.market.repository.OrderItemRepository;
 import com.alex.market.repository.OrderRepository;
 import com.alex.market.repository.projection.OrderItemsDetails;
 import com.alex.market.service.impl.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -17,6 +20,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.List;
@@ -28,57 +32,87 @@ import static org.mockito.Mockito.when;
 
 @SpringJUnitConfig
 class OrderServiceTest {
-    private final Long VALID_ID=1L;
-    private final Long INVALID_ID=1000000L;
+    private final Long VALID_ID = 1L;
+    private final Long INVALID_ID = 1000000L;
     @Autowired
     private OrderService orderService;
     @Autowired
     private OrderItemRepository orderItemRepository;
-   /* @Autowired
-    private OrderMapper orderMapper;*/
+    @Autowired
+    private ItemMapper itemMapper;
+
+    /* @Autowired
+     private OrderMapper orderMapper;*/
     @Autowired
     private OrderRepository orderRepository;
 
     private Map<Long, Integer> cartItemsCount;
     private ItemDto itemDto;
+    private OrderItemsDetails orderItemsDetails;
 
     @BeforeEach
     void setUp() {
         cartItemsCount = new HashMap<>();
         cartItemsCount.put(VALID_ID, 1);
-        itemDto=new ItemDto(VALID_ID, "testTitle1", "testDesc1", "testImagePath1", 1000L, 1);
+        itemDto = new ItemDto(VALID_ID, "testTitle1", "testDesc1", "testImagePath1", 1000L, 1);
+        orderItemsDetails = new OrderItemsDetails(VALID_ID, itemDto.title(), itemDto.description(), itemDto.imgPath(), itemDto.price(), itemDto.count());
     }
 
     @Test
     void getAllOrders_shouldReturnOrderDtoListSuccess() {
-        OrderDto orderDto = new OrderDto(VALID_ID, List.of(itemDto),1000L);
-        List<OrderItemsDetails> listDetails=List.of(new OrderItemsDetails(VALID_ID,itemDto.title(),itemDto.description(),itemDto.imgPath(),itemDto.price(),itemDto.count()));
-       // when(orderMapper.toDto(any(Order.class))).thenReturn(orderDto);
+        OrderDto orderDto = new OrderDto(VALID_ID, List.of(itemDto), 1000L);
         when(orderRepository.findAll()).thenReturn(Flux.fromIterable(List.of(Order.builder().id(VALID_ID).totalSum(1000L).build())));
-        when(orderItemRepository.findItemsWithDetailsByOrderId(VALID_ID)).thenReturn(Flux.fromIterable(listDetails));
+        when(orderItemRepository.findItemsWithDetailsByOrderId(VALID_ID)).thenReturn(Flux.fromIterable(List.of(orderItemsDetails)));
+        when(itemMapper.toDtoFromOrderItemDetails(orderItemsDetails)).thenReturn(itemDto);
 
-        List<OrderDto> actualDto=orderService.findAllOrders().collectList().block();
+        List<OrderDto> actualDto = orderService.findAllOrders().collectList().block();
 
         assertThat(actualDto).hasSize(1).contains(orderDto);
+    }
+
+    @Test
+    void findOrderWithItems_shouldReturnOrderDtoByIdSuccess() {
+        OrderDto orderDto = new OrderDto(VALID_ID, List.of(itemDto), 1000L);
+        Order order = Order.builder().id(VALID_ID).totalSum(1000L).build();
+        when(orderRepository.findById(VALID_ID)).thenReturn(Mono.just(order));
+        when(orderItemRepository.findItemsWithDetailsByOrderId(VALID_ID)).thenReturn(Flux.fromIterable(List.of(orderItemsDetails)));
+        when(itemMapper.toDtoFromOrderItemDetails(orderItemsDetails)).thenReturn(itemDto);
+
+        OrderDto actualOrderDto = orderService.findOrderWithItems(VALID_ID).block();
+
+        Assertions.assertThat(actualOrderDto).isEqualTo(orderDto);
+    }
+
+    @Test
+    void findOrderWithItems_shouldThrowOrderNotFoundException_whenMonoIsEmptyFail() {
+
+        when(orderRepository.findById(INVALID_ID)).thenReturn(Mono.empty());
+        when(orderItemRepository.findItemsWithDetailsByOrderId(INVALID_ID)).thenReturn(Flux.fromIterable(List.of(orderItemsDetails)));
+
+        Assertions.assertThatExceptionOfType(OrderNotFoundException.class)
+                .isThrownBy(()-> orderService.findOrderWithItems(INVALID_ID).block());
     }
 
     @TestConfiguration
     static class OrderServiceTestContextConfiguration {
         @Bean
-        public OrderService orderService(OrderRepository orderRepository,OrderItemRepository orderItemRepository,ItemMapper itemMapper) {
-            return new OrderServiceImpl(orderRepository,orderItemRepository,itemMapper);
+        public OrderService orderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository, ItemMapper itemMapper) {
+            return new OrderServiceImpl(orderRepository, orderItemRepository, itemMapper);
         }
+
         @Bean
         public OrderRepository orderRepository() {
             return Mockito.mock(OrderRepository.class);
         }
+
         @Bean
         public OrderItemRepository orderItemRepository() {
             return Mockito.mock(OrderItemRepository.class);
         }
+
         @Bean
         public ItemMapper itemMapper() {
-            return new ItemMapperImpl();
+            return Mockito.mock(ItemMapper.class);
         }
     }
 
