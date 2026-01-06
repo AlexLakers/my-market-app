e8468a21b5f5c2191dc048afaefddf349a3927d0
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-01-06 13:20:41 +0300

    I added my custom repository 'QueryOrderItemRepository' with method 'findItemsWithDetailsByOrderId()' to find all orders with OrderItems details.

---
+++ src/main/java/com/alex/market/repository/QueryOrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.alex.market.repository;
+
+import com.alex.market.repository.projection.OrderItemsDetails;
+import reactor.core.publisher.Flux;
+
+public interface QueryOrderItemRepository {
+
+    Flux<OrderItemsDetails> findItemsWithDetailsByOrderId(Long orderId);
+}
