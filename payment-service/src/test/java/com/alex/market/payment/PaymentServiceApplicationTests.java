3abd135dda3f52a2c40a73b1ec422b2ee6121b38
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-01-14 07:13:17 +0300

    I crated multimodule project: 1. Added payment-service module as a RESTfull service

---
+++ payment-service/src/test/java/com/alex/market/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.market.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
