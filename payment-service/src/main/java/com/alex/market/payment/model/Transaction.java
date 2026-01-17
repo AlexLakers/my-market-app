4edf207590d60f37269de49b7587a47f41322687
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-01-17 17:04:45 +0300

    1.I OpenApi Added FailureReason field to inform user about error in transaction. 2. Added static methods failed and success for more information.

--- payment-service/src/main/java/com/alex/market/payment/model/Transaction.java
+++ payment-service/src/main/java/com/alex/market/payment/model/Transaction.java
@@ -2,6 +2,7 @@ package com.alex.market.payment.model;
 
 import lombok.*;
 import lombok.experimental.FieldNameConstants;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
@@ -32,16 +33,46 @@ public class Transaction {
     @Column("amount")
     private Long amount;
 
-    @Column("new_balance")
-    private Long newBalance;
-
     @Column("status")
     private TransactionStatus status;
 
     @Column("type")
     private TransactionType type;
 
+    @Column("failure_reason")
+    private String failureReason;
+
     @Column("created_at")
     private LocalDateTime createdAt;
 
+    public static Transaction success(Long accountId,
+                                      Long orderId,
+                                      Long amount,
+                                      TransactionType type) {
+        return Transaction.builder()
+                .status(TransactionStatus.SUCCESS)
+                .accountId(accountId)
+                .orderId(orderId)
+                .amount(amount)
+                .type(type)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static Transaction failed(Long accountId,
+                                     Long orderId,
+                                     Long amount,
+                                     TransactionType type) {
+        return Transaction.builder()
+                .status(TransactionStatus.FAILED)
+                .accountId(accountId)
+                .orderId(orderId)
+                .amount(amount)
+                .type(type)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+
 }
+
