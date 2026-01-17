4d1ff369c0efa94edbe1ea2b0314b6ce9f6c4d27
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-01-17 22:13:09 +0300

    I added TransactionRepository: 1.Added method TransactionRepository#findByOrderId() using auto generate realization by name.

--- payment-service/src/main/java/com/alex/market/payment/repository/QueryAccountRepository.java
+++ payment-service/src/main/java/com/alex/market/payment/repository/QueryAccountRepository.java
@@ -3,7 +3,6 @@ package com.alex.market.payment.repository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
-
 public interface  QueryAccountRepository {
     Mono<Long> decrementBalanceAtomic(Long id, Long amount);
 }
