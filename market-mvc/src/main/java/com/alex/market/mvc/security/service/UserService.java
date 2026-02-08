4454c5d7dd00bf93f9411314520d029488af48d3
AlexLakers <alexey.nefedov199321@yandex.ru>
2026-02-08 10:54:01 +0300

    1. I added CustomUserDetails with userId; 2. I extend 'UserService' using 'ReactiveUserDetailsService' and realize method for specific Authentication provider.

--- market-mvc/src/main/java/com/alex/market/mvc/security/service/UserService.java
+++ market-mvc/src/main/java/com/alex/market/mvc/security/service/UserService.java
@@ -2,8 +2,10 @@ package com.alex.market.mvc.security.service;
 
 import com.alex.market.mvc.security.dto.UserDto;
 import com.alex.market.mvc.security.dto.UserRegDto;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import reactor.core.publisher.Mono;
 
-public interface UserService{
+public interface UserService extends ReactiveUserDetailsService{
     Mono<UserDto> createUser(UserRegDto userRegDto);
 }
--- market-mvc/src/main/java/com/alex/market/mvc/security/service/UserServiceImpl.java
+++ market-mvc/src/main/java/com/alex/market/mvc/security/service/UserServiceImpl.java
@@ -4,15 +4,20 @@ import com.alex.market.mvc.security.dto.UserDto;
 import com.alex.market.mvc.security.dto.UserRegDto;
 import com.alex.market.mvc.security.exception.UsernameAlreadyExists;
 import com.alex.market.mvc.security.mapper.UserMapper;
+import com.alex.market.mvc.security.model.CustomUserDetails;
 import com.alex.market.mvc.security.model.Role;
 import com.alex.market.mvc.security.model.User;
 import com.alex.market.mvc.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -41,5 +46,15 @@ public class UserServiceImpl implements UserService {
 
     private String encodePassword(String rawPassword) {
         return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public Mono<UserDetails> findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .switchIfEmpty(Mono.error(new UsernameNotFoundException(username)))
+                .map(user -> {
+                    log.info("User with id: {} and username: {}  is found", user.getId(), user.getUsername());
+                    return new CustomUserDetails( user.getUsername(), user.getPassword(), Collections.singletonList(user.getRole()),user.getId());
+                });
     }
 }
